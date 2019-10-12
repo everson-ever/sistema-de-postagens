d69b7f2a1bf6554b2a9e45a591486399d7d3667d
Everson Silva <eversonsilva9799@gmail.com>
2019-10-12 16:37:03 -0300

    Model para mensagens http criado

--- conversa_back/src/app/controllers/ComentarioController.js
+++ conversa_back/src/app/controllers/ComentarioController.js
@@ -1,11 +1,12 @@
 const Comentario = require('../models/Comentario');
+const HttpStatus = require('../models/HttpStatus');
 
 class ComentarioController {
 	async index(req, res) {
 		const { id } = req.params;
 
 		let comentarios = await Comentario.select(id);
-		return res.status(200).json(comentarios[0]);
+		return res.status(HttpStatus.ok).json(comentarios[0]);
 	}
 
 	async store(req, res) {
@@ -15,9 +16,9 @@ class ComentarioController {
 
 		if (inserted.affectedRows === 1) {
 			req.io.emit('comentario', { comentario, Autor: req.nomeUsuario, idPost });
-			return res.status(201).json({ status: true });
+			return res.status(HttpStatus.created).json({ status: true });
 		}
-		return res.status(500).json({ status: false, message: 'Internal Server Error' });
+		return res.status(HttpStatus.internalServerError).json({ status: false, message: 'Internal Server Error' });
 	}
 }
 
